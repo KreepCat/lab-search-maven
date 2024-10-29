0148bca65ba3cd0924dc3149fec0b6fcc7acc825
AlexPollock <pollocka@grinnell.edu>
2024-10-29 15:21:38 -0500

    Add systematic tests of binary search.

--- src/test/java/edu/grinnell/csc207/TestSearch.java
+++ src/test/java/edu/grinnell/csc207/TestSearch.java
@@ -26,41 +26,31 @@ public class TestSearch {
   /**
    * A string version of a call to binary search.
    *
-   * @param values
-   *   The array.
-   * @param val
-   *   The value we're searching for.
+   * @param values The array.
+   * @param val The value we're searching for.
    *
-   * @return
-   *   The string "binarySearch(values, val)"
+   * @return The string "binarySearch(values, val)"
    */
   String bsCall(int[] values, int val) {
     return String.format("binarySearch(%s, %d)", Arrays.toString(values), val);
   } // bsCall
 
   /**
-   * Assert that a search for a particular value finds the value at an
-   * expected index.
+   * Assert that a search for a particular value finds the value at an expected index.
    *
-   * @param expected
-   *   The expected index.
-   * @param values
-   *   The array we're searching.
-   * @param val
-   *   The value we're searching for.
+   * @param expected The expected index.
+   * @param values The array we're searching.
+   * @param val The value we're searching for.
    */
-  void assertBinarySearchFinds(int expected, int[] values, int val) 
-      throws Exception {
-    assertEquals(expected, SearchUtils.binarySearch(values, val),
-        () -> bsCall(values, val));
+  void assertBinarySearchFinds(int expected, int[] values, int val) throws Exception {
+    assertEquals(expected, SearchUtils.binarySearch(values, val), () -> bsCall(values, val));
   } // assertBinarySearchFinds(int, int[], int)
 
   /**
    * Assert that a search for a particular value finds the value.
-   * @param values
-   *   The array we're searching.
-   * @param val
-   *   The value we're searching for.
+   * 
+   * @param values The array we're searching.
+   * @param val The value we're searching for.
    */
   void assertBinarySearchFinds(int[] values, int val) throws Exception {
     assertEquals(val, values[SearchUtils.binarySearch(values, val)],
@@ -68,17 +58,14 @@ public class TestSearch {
   } // assertBinarySearchFinds(int[], int)
 
   /**
-   * Assert that a search for a particular value fails (hopefully, because 
-   * the value * is not in the array).
+   * Assert that a search for a particular value fails (hopefully, because the value * is not in the
+   * array).
    *
-   * @param values
-   *   The array we're searching.
-   * @param val
-   *   The value we're searching for.
+   * @param values The array we're searching.
+   * @param val The value we're searching for.
    */
   void assertBinarySearchFails(int[] values, int val) throws Exception {
-    assertThrows(Exception.class,
-        () -> SearchUtils.binarySearch(values, val),
+    assertThrows(Exception.class, () -> SearchUtils.binarySearch(values, val),
         () -> bsCall(values, val));
   } // assertBinarySearchFails()
 
@@ -91,7 +78,7 @@ public class TestSearch {
    */
   @Test
   void testBinarySearchEmpty() throws Exception {
-    assertBinarySearchFails(new int[] { }, 0);
+    assertBinarySearchFails(new int[] {}, 0);
   } // testSearchEmpty()
 
   /**
@@ -99,9 +86,9 @@ public class TestSearch {
    */
   @Test
   void testBinarySearchOne() throws Exception {
-    assertBinarySearchFinds(0, new int[] { 5 }, 5);
-    assertBinarySearchFails(new int[] { 5 }, 0);
-    assertBinarySearchFails(new int[] { 5 }, 10);
+    assertBinarySearchFinds(0, new int[] {5}, 5);
+    assertBinarySearchFails(new int[] {5}, 0);
+    assertBinarySearchFails(new int[] {5}, 10);
   } // testBinarySearchOne()
 
   /**
@@ -109,11 +96,11 @@ public class TestSearch {
    */
   @Test
   void testBinarySearchTwo() throws Exception {
-    assertBinarySearchFinds(0, new int[] { 7, 11 }, 7);
-    assertBinarySearchFinds(1, new int[] { 7, 11 }, 11);
-    assertBinarySearchFails(new int[] { 7, 11 }, 0);
-    assertBinarySearchFails(new int[] { 7, 11 }, 10);
-    assertBinarySearchFails(new int[] { 7, 11 }, 20);
+    assertBinarySearchFinds(0, new int[] {7, 11}, 7);
+    assertBinarySearchFinds(1, new int[] {7, 11}, 11);
+    assertBinarySearchFails(new int[] {7, 11}, 0);
+    assertBinarySearchFails(new int[] {7, 11}, 10);
+    assertBinarySearchFails(new int[] {7, 11}, 20);
   } // testBinarySearchTwo()
 
   /**
@@ -121,18 +108,39 @@ public class TestSearch {
    */
   @Test
   void testBinarySearchDups() throws Exception {
-    assertBinarySearchFinds(new int[] { 1, 1, 1, 2, 2, 3 }, 1);
-    assertBinarySearchFinds(new int[] { 1, 1, 1, 2, 2, 3 }, 2);
-    assertBinarySearchFinds(new int[] { 1, 1, 1, 2, 2, 3 }, 3);
+    assertBinarySearchFinds(new int[] {1, 1, 1, 2, 2, 3}, 1);
+    assertBinarySearchFinds(new int[] {1, 1, 1, 2, 2, 3}, 2);
+    assertBinarySearchFinds(new int[] {1, 1, 1, 2, 2, 3}, 3);
   } // testBinarySearchDups()
 
   @Test
   void testBinarySearchThree() throws Exception {
-    assertBinarySearchFinds(1, new int[] { 2, 7, 11 }, 7);
-    assertBinarySearchFinds(1, new int[] { 7, 11, 12, 13, 14 }, 11);
-    assertBinarySearchFails(new int[] { 7, 11, 15 }, 0);
-    assertBinarySearchFails(new int[] { 7, 11 }, 10);
-    assertBinarySearchFails(new int[] { 7, 11 }, 20);
+    assertBinarySearchFinds(1, new int[] {2, 7, 11}, 7);
+    assertBinarySearchFinds(1, new int[] {7, 11, 12, 13, 14}, 11);
+    assertBinarySearchFails(new int[] {7, 11, 15}, 0);
+    assertBinarySearchFails(new int[] {7, 11}, 10);
+    assertBinarySearchFails(new int[] {7, 11}, 20);
   } // testBinarySearchTwo()
+
+  @Test
+  void testBinarySearchFour() throws Exception {
+
+    for (int i = 1; i < 32; i++) {
+      int newArray[] = new int[i];
+      for (int j = 0; j < i; j++) {
+        newArray[j] = j * 2;
+      }
+      for (int s = 0; s < i; s++) {
+        // Make sure that value 2*i is in position i
+        assertBinarySearchFinds(s, newArray, 2 * s);
+
+        // Make sure that odd values are not in the array
+        assertBinarySearchFails(newArray, (2 * s) + 1);
+      }
+
+    }
+
+
+  }
 
 } // class TestSearch
