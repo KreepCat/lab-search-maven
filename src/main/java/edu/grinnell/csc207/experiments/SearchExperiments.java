2c26dd08f9f7086afa0f461732feeea2a18e61d4
AlexPollock <pollocka@grinnell.edu>
2024-10-29 14:27:42 -0500

    Implement search method for iterables.

--- src/main/java/edu/grinnell/csc207/experiments/SearchExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/SearchExperiments.java
@@ -1,39 +1,60 @@
 package edu.grinnell.csc207.experiments;
 
 import java.io.PrintWriter;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import java.util.function.Predicate;
-
 import edu.grinnell.csc207.util.SearchUtils;
+import java.util.function.Predicate;
 
 /**
  * Assorted experiments for searching structures.
  *
- * @author Your Name Here
- * @author Your Name Here
+ * @author SJ Kim
+ * @author Alex Pollock
  * @author Samuel A. Rebelsky (starter code)
  */
 public class SearchExperiments {
   /**
    * Run our experimens.
    *
-   * @param args
-   *   Command-line arguments. Ignored.
+   * @param args Command-line arguments. Ignored.
    */
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
 
-    String[] tmp =
-        new String[] { "alpha", "bravo", "charlie", "delta", "echo",
-                       "foxtrot", "golf", "hotel", "india",
-                       "juliett", "kilo", "lima", "mike",
-                       "november", "oscar", "papa", "quebec",
-                       "romeo", "sierra", "tango", "uniform",
-                       "victor", "whiskey", "xray", "yankee", "zulu" };
+    String[] tmp = new String[] {"alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf",
+        "hotel", "india", "juliett", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec",
+        "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu"};
     ArrayList<String> strings = new ArrayList<String>(Arrays.asList(tmp));
+
+
+
+    try {
+      Predicate<String> lessThanFiveChars = new Predicate<String>() {
+        public boolean test(String str) {
+          return (str.length() < 5);
+        } // test(String)
+      };
+      String ex1c = SearchUtils.search(strings, lessThanFiveChars);
+      pen.println("The first string of fewer than five letters is " + ex1c);
+    } catch (Exception e) {
+      pen.println("There are no strings of fewer than five letters.");
+    } // try/catch
+
+
+    try {
+      String ex1g = SearchUtils.search(strings, (s) -> s.length() == 6);
+      pen.println("The first string of exactly six letters is " + ex1g);
+    } catch (Exception e) {
+      pen.println("There are no strings of exactly six letters.");
+    } // try/catch
+
+    try {
+      String ex1g = SearchUtils.search(strings, (s) -> s.contains("u"));
+      pen.println("first thing with u" + ex1g);
+    } catch (Exception e) {
+      pen.println("its an element with no u");
+    } // try/catch
 
     pen.close();
   } // main(String[])
--- src/main/java/edu/grinnell/csc207/util/SearchUtils.java
+++ src/main/java/edu/grinnell/csc207/util/SearchUtils.java
@@ -17,69 +17,48 @@ public class SearchUtils {
   /**
    * Search for val in values, return the index of an instance of val.
    *
-   * @param values
-   *   A sorted array of integers
-   * @param val
-   *   An integer we're searching for
-   * @return
-   *   index, an index of val (if one exists)
-   * @throws Exception
-   *   If there is no i s.t. values[i] == val
-   * @pre
-   *   values is sorted in increasing order.  That is, values[i] <=
-   *   values[i+1] for all reasonable i.
-   * @post
-   *   values[index] == val
+   * @param values A sorted array of integers
+   * @param val An integer we're searching for
+   * @return index, an index of val (if one exists)
+   * @throws Exception If there is no i s.t. values[i] == val
+   * @pre values is sorted in increasing order. That is, values[i] <= values[i+1] for all reasonable
+   *      i.
+   * @post values[index] == val
    */
   static int iterativeBinarySearch(int[] vals, int val) throws Exception {
-    return 0;   // STUB
+    return 0; // STUB
   } // iterativeBinarySearch
 
   /**
    * Search for val in values, return the index of an instance of val.
    *
-   * @param values
-   *   A sorted array of integers
-   * @param val
-   *   An integer we're searching for
-   * @return
-   *   index, an index of val (if one exists)
-   * @throws Exception
-   *   If there is no i s.t. values[i] == val
-   * @pre
-   *   values is sorted in increasing order.  That is, values[i] <=
-   *   values[i+1] for all reasonable i.
-   * @post
-   *   values[index] == val
+   * @param values A sorted array of integers
+   * @param val An integer we're searching for
+   * @return index, an index of val (if one exists)
+   * @throws Exception If there is no i s.t. values[i] == val
+   * @pre values is sorted in increasing order. That is, values[i] <= values[i+1] for all reasonable
+   *      i.
+   * @post values[index] == val
    */
   static int recursiveBinarySearch(int[] vals, int val) throws Exception {
-    return 0;   // STUB
+    return 0; // STUB
   } // recursiveBinarySearch
 
   /**
-   * Search for val in a subarray of values, return the index of an 
-   * instance of val.
+   * Search for val in a subarray of values, return the index of an instance of val.
    *
-   * @param values
-   *   A sorted array of integers
-   * @param lb
-   *   The lower bound of the area of interest (inclusive)
-   * @param ub
-   *   The upper bound of the area of interest (exclusive)
-   * @param val
-   *   An integer we're searching for
-   * @return
-   *   index, an index of val (if one exists)
-   * @throws Exception
-   *   If there is no i between lb and ub s.t. values[i] == val
-   * @pre
-   *   values is sorted in increasing order.  That is, values[i] <=
-   *   values[i+1] for all reasonable i.
-   * @post
-   *   values[index] == val
+   * @param values A sorted array of integers
+   * @param lb The lower bound of the area of interest (inclusive)
+   * @param ub The upper bound of the area of interest (exclusive)
+   * @param val An integer we're searching for
+   * @return index, an index of val (if one exists)
+   * @throws Exception If there is no i between lb and ub s.t. values[i] == val
+   * @pre values is sorted in increasing order. That is, values[i] <= values[i+1] for all reasonable
+   *      i.
+   * @post values[index] == val
    */
   static int rbsHelper(int[] vals, int lb, int ub, int val) throws Exception {
-    return 0;   // STUB
+    return 0; // STUB
   } // rbsHelper
 
   // +----------------+----------------------------------------------
@@ -89,40 +68,33 @@ public class SearchUtils {
   /**
    * Search values for the first value for which pred holds.
    *
-   * @param <T> 
-   *   The type of values we're examining
-   * @param values
-   *   The iterable we're searching
-   * @param pred
-   *   The predicate used to determine whether or not the value is
-   *   acceptable
+   * @param <T> The type of values we're examining
+   * @param values The iterable we're searching
+   * @param pred The predicate used to determine whether or not the value is acceptable
    * 
    * @return the first mathcing element.
    *
-   * @throws Exception
-   *   If no matching value is found.
+   * @throws Exception If no matching value is found.
    */
-  public static <T> T search(Iterable<T> values, Predicate<? super T> pred) 
-      throws Exception {
-    throw new Exception("Unimplemented");       // STUB
+  public static <T> T search(Iterable<T> values, Predicate<? super T> pred) throws Exception {
+    for (T value : values){
+      if (pred.test(value)){
+        return value;
+      } // if
+    } // for
+    throw new Exception();
   } // search(Iterable<T>, Predicate<? super T>)
 
   /**
    * Search for val in values, return the index of an instance of val.
    *
-   * @param values
-   *   A sorted array of integers
-   * @param val
-   *   An integer we're searching for
-   * @return
-   *   index, an index of val (if one exists)
-   * @throws Exception
-   *   If there is no i s.t. values[i] == val
-   * @pre
-   *   values is sorted in increasing order.  That is, values[i] <=
-   *   values[i+1] for all reasonable i.
-   * @post
-   *   values[index] == val
+   * @param values A sorted array of integers
+   * @param val An integer we're searching for
+   * @return index, an index of val (if one exists)
+   * @throws Exception If there is no i s.t. values[i] == val
+   * @pre values is sorted in increasing order. That is, values[i] <= values[i+1] for all reasonable
+   *      i.
+   * @post values[index] == val
    */
   public static int binarySearch(int[] vals, int val) throws Exception {
     return 0;
